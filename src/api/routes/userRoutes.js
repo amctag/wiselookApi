66397f249c7f74461926f650fdaed8c507ffcaa8
amctag <69043097+amctag@users.noreply.github.com>
2025-05-21 12:47:48 +0300

    Update userRoutes.js

--- src/api/routes/userRoutes.js
+++ src/api/routes/userRoutes.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const { getAllUsers, getUserById } = require('/app/src/api/controllers/userController');
 
-// GET /users
-router.get('/users', userController.getAllUsers);
-
-// GET /users/:id
-router.get('/:id', getUserById);
-
-module.exports = router;
