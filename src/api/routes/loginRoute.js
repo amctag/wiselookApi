fd120c436fc16635ec3e396fe8cd44f0a338f71f
amctag <69043097+amctag@users.noreply.github.com>
2025-05-21 11:34:12 +0300

    Update loginRoute.js

--- src/api/routes/loginRoute.js
+++ src/api/routes/loginRoute.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
-const loginController = require('../../controllers/loginController');
+const loginController = require('app/src/api/controllers/loginController');
 
 // Validation rules
 const loginValidation = [
