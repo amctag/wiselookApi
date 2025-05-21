996ea27570e7783a7aae1c77a7373d20c62352b6
amctag <69043097+amctag@users.noreply.github.com>
2025-05-21 13:53:42 +0300

    Update userValidation.js

--- src/api/middleware/userValidation.js
+++ src/api/middleware/userValidation.js
@@ -1,3 +1,17 @@
+console.log('📦 Loading uservalidate.js');
+const path = require('path');
+require('dotenv').config({ path: path.join(process.cwd(), 'src', 'api', 'routes', '.env') });
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT || 5011,
+  ssl: process.env.DB_SSL === "true"
+});
 // src/api/middleware/userValidation.js
 const Joi = require('joi');
 
